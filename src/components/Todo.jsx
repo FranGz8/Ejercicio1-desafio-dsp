29948c883685691a15e9a3e447e815866c8134b0
FranGz8 <0javalife@gmail.com>
2021-09-03 13:27:38 -0600

    lista funcionando

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Todo = ({todo, index, deleteTodo, cambioCantidad, cantidad}) => {
+import Producto from '../productos.json'
+
+const Todo = ({ cantidad, index, deleteTodo, cambioCantidad}) => {
     return (
         <>
             <div className="list">
-                <h3>{todo}</h3> <input type="text" className="cantidad" onChange={() => cambioCantidad(index)} /> <button className="btn-delete" onClick={() => deleteTodo(index)}>x</button>
-                <p className="precio">{cantidad}</p>
+                <h3>{Producto[index]['nombre']}</h3>
+                <input type="number" className="cantidad" min='1' value={cantidad} onChange={e => cambioCantidad(e.target.value, index)} />
+                <button className="btn-delete" onClick={() => deleteTodo(index)}>x</button>
+                <br /><p className="precio">{Producto[index]['precio']}</p> 
             </div>
             <hr/>
         </>
